638cd54cc72fc2f4d3315c5b4ba80a58fef33069
Bogdan <bogdan9110@gmail.com>
2022-10-04 09:16:20 +0300

    edit footer text

--- Global_Ukraine/global-ukraine-page/src/scripts/form.js
+++ Global_Ukraine/global-ukraine-page/src/scripts/form.js
@@ -1,13 +1,13 @@
-const allowedNameCountryUa = ['Австрія', 'Бельгія', 'Болгарія', 'Хорватія', 'Кіпр', 'Чехія', 'Данія', 'Естонія', 'Фінляндія', 'Франція', ' Німеччина', 'Греція', 'Угорщина', 'Ірландія', 'Італія', 'Латвія', 'Литва', 'Люксембург', 'Мальта', 'Нідерланди', 'Польща', 'Португалія', 'Румунія', 'Словаччина', 'Словенія', 'Іспанія', 'Швеція', 'Україна', 'Ісландія', 'Ліхтенштейн', 'Норвегія'];
-const allowedNameCountryEn = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Ukraine', 'Sceland', 'Liechtenstein', 'Norway'];
 
 const allowedNameCountry = [{ ua: 'Австрія', en: 'Austria' }, { ua: 'Бельгія', en: 'Belgium' }, { ua: 'Болгарія', en: 'Bulgaria' }, { ua: 'Хорватія', en: 'Croatia' }, { ua: 'Кіпр', en: 'Cyprus' }, { ua: 'Чехія', en: 'Czech Republic' }, { ua: 'Данія', en: 'Denmark' }, { ua: 'Естонія', en: 'Estonia' }, { ua: 'Фінляндія', en: 'Finland' }, { ua: 'Франція', en: 'France' }, { ua: 'Німеччина', en: 'Germany' }, { ua: 'Греція', en: 'Greece' }, { ua: 'Угорщина', en: 'Hungary' }, { ua: 'Ірландія', en: 'Ireland' }, { ua: 'Італія', en: 'Italy' }, { ua: 'Латвія', en: 'Latvia' }, { ua: 'Литва', en: 'Lithuania' }, { ua: 'Люксембург', en: 'Luxembourg' }, { ua: 'Мальта', en: 'Malta' }, { ua: 'Нідерланди', en: 'Netherlands' }, { ua: 'Польща', en: 'Poland' }, { ua: 'Португалія', en: 'Portugal' }, { ua: 'Румунія', en: 'Romania' }, { ua: 'Словаччина', en: 'Slovakia' }, { ua: 'Словенія', en: 'Slovenia' }, { ua: 'Іспанія', en: 'Spain' }, { ua: 'Швеція', en: 'Sweden' }, { ua: 'Україна', en: 'Ukraine' }, { ua: 'Ісландія', en: 'Sceland' }, { ua: 'Ліхтенштейн', en: 'Liechtenstein' }, { ua: 'Норвегія', en: 'Norway' }];
 const DefaultValueSelectUa = 'Вибрати країну...';
-const DefaultValueSelectEn = 'Choose other country...';
+const DefaultValueSelectEn = 'Choose country...';
 let hash = window.location.hash;
 hash = hash.substr(1);
 const inpName = document.querySelector('#name').querySelector('input');
-hash === 'ua' ? inpName.placeholder = 'Гліб Українець' : inpName.placeholder = 'Hlib Ukrainets';
+const inpEmail = document.querySelector('#email').querySelector('input');
+hash === 'ua' ? inpName.placeholder = `Введіть Ім'я та прізвище` : inpName.placeholder = 'Enter first name and last name';
+hash === 'ua' ? inpEmail.placeholder = `Введіть пошту` : inpEmail.placeholder = 'Enter your email';
 
 let DefaultValueSelectLang = '';
 hash === 'ua' ? DefaultValueSelectLang = DefaultValueSelectUa : DefaultValueSelectLang = DefaultValueSelectEn
@@ -28,15 +28,17 @@ const refs = {
 	selectCustomTrigger: document.querySelector('.selectCustom-trigger'),
 	loader: document.querySelector('.loader'),
 	fakeButton: document.querySelector('.fake-btn'),
+
 }
 const translateModal = {
 	successFirstTitle: {
-		ua: 'Ваші дані надіслані успішно!',
-		en: 'Your data has been sent successfully!',
+		ua: 'Дякуємо ! Ваші дані надіслані успішно !',
+		en: 'Thank you! Your data has been sent successfully!',
 	},
 	successSecondTitle: {
-		ua: 'Очікуйте відповіді...',
-		en: 'Wait for the answer...',
+		ua: `Найближчим часом, ми повідомимо, коли Ви зможете замовити брендовану карту від Global Ukraine.
+		<br>Наша унікальна карта закодована на перемогу !`,
+		en: `In the near future, we will inform you when you can order a branded card from Global Ukraine.<br>Our unique card is coded for victory!`,
 	},
 	successATitle: {
 		ua: 'Завершити',
@@ -57,7 +59,12 @@ const translateModal = {
 }
 refs.selectCustomTrigger.innerText = DefaultValueSelectLang;
 const getSelectMurcup = () => {
-	const arrMurcupSelectItem = hash === 'ua' ? allowedNameCountry.map(item => `<div class="selectCustom-option">${item.ua}</div>`) : allowedNameCountry.map(item => `<div class="selectCustom-option">${item.en}</div>`);
+	const customSortedFn = (a, b) => {
+		let nameA = a[`${hash}`].toLowerCase();
+		let nameB = b[`${hash}`].toLowerCase();
+		return nameA.localeCompare(nameB);
+	}
+	const arrMurcupSelectItem = hash === 'ua' ? allowedNameCountry.sort(customSortedFn).map(item => `<div class="selectCustom-option">${item.ua}</div>`) : allowedNameCountry.sort(customSortedFn).map(item => `<div class="selectCustom-option">${item.en}</div>`);
 	return arrMurcupSelectItem.join("");
 }
 refs.selectCustomOptions.innerHTML = getSelectMurcup();
@@ -71,9 +78,40 @@ const extractData = (formElement) => {
 
 	const checkboxFirst = ['Індивідуальний (фізособа)', formElement[2].checked];
 	const checkboxSecond = [' Корпоративний (ФОП)', formElement[3].checked];
-	return { name, email, country, checkboxFirst, checkboxSecond };
+	const captcha = document.querySelector("#g-recaptcha-response").value;
+	return { name, email, country, checkboxFirst, checkboxSecond, captcha };
+}
+function getSecondsToday() {
+	let now = new Date();
+	let today = localStorage.getItem("time");
+
+	let diff = now - today;
+	const isDay = Math.round(diff / 1000 / 86400);
+	if (isDay <= 1) return true;
+	return false;
 }
 
+const formTimeRegulatorBefore = () => {
+	if (localStorage.getItem('times') >= 3) {
+		if (getSecondsToday()) {
+			alert(`Ви вечерпали ліміт надсилання форм, ви знову змоежет надсилати через 24 год.`);
+			return false;
+		} else {
+			localStorage.setItem('times', 1);
+			localStorage.setItem('time', new Date());
+		}
+	}
+	return true;
+}
+const formTimeRegulator = () => {
+	if (localStorage.getItem('times') === null) {
+		localStorage.setItem('times', 1);
+		localStorage.setItem('time', new Date());
+		return;
+	}
+	const currentTimes = localStorage.getItem('times');
+	localStorage.setItem('times', Number(currentTimes) + 1);
+}
 const verifyName = (nameStr) => {
 	if (nameStr.length === 0) {
 		return hash === 'ua' ? 'Це поле не повинно бути пустим!' : 'This field must not be empty!';
@@ -92,6 +130,10 @@ const verifyCountry = () => {
 }
 const verifyCheckbox = (checkboxes) => {
 	if (checkboxes.indexOf(true) === -1) return hash === 'ua' ? 'Виберіть хоча б один із запропонованих варіантів' : 'Choose at least one of the proposed options';
+	return false;
+}
+const verifyCaptcha = (captchaValue) => {
+	if (!captchaValue) return hash === 'ua' ? 'Підтвердіть Каптчу' : 'Check the CAPTCHA';
 	return false;
 }
 const cleanErrors = (elementsToClean) => elementsToClean.forEach(item => item.innerText = '');
@@ -126,6 +168,7 @@ const errorWindowOpen = (error) => {
 const successWindowOpen = (post) => {
 	refs.modalContent.innerHTML = '';
 	if (post.status === 200) {
+		formTimeRegulator();
 		refs.modalContent.innerHTML = getMurcup(true);
 		refs.modal.style.opacity = 1;
 	} else errorWindowOpen('error');
@@ -147,14 +190,43 @@ const toggleLoader = (isWork) => {
 		refs.formButton.style.display = 'block';
 	}
 }
+const cleanInputs = (form) => {
+	form[0].value = '';
+	form[1].value = '';
+	form[2].checked = false;
+	form[3].checked = false;
+	refs.selectCustomTrigger.innerText = DefaultValueSelectLang;
+}
+const fetchPostData = (postToAdd, element) => {
+	try {
+		fetch('https://global-ukraine-card.herokuapp.com/', {
+			method: 'POST',
+			body: JSON.stringify(postToAdd),
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8',
+			},
+		})
+			.then(response => response.json())
+			.then(post => successWindowOpen(post))
+			.catch(error => errorWindowOpen(error))
+			.finally(() => {
+				toggleLoader(false)
+				cleanInputs(element);
+			});
+	} catch (e) {
+		errorWindowOpen(error);
+	}
+}
+
 const formHandler = (e) => {
 	e.preventDefault();
+	if (!formTimeRegulatorBefore()) return;
 	const nameElement = document.querySelector('#name').querySelector('.error');
 	const emailElement = document.querySelector('#email').querySelector('.error');
 	const countryElement = document.querySelector('#country').querySelector('.error');
 	const checkboxElement = document.querySelector('#checkbox').querySelector('.error');
-
-	cleanErrors([nameElement, emailElement, countryElement, checkboxElement]);
+	const captchaElement = document.querySelector('#captcha-container-custom-go').querySelector('.error');
+	cleanErrors([nameElement, emailElement, countryElement, checkboxElement, captchaElement]);
 
 	const element = e.currentTarget;
 	console.dir(e.currentTarget);
@@ -163,12 +235,13 @@ const formHandler = (e) => {
 	const msgErrorName = verifyName(data.name);
 	const msgErrorCountry = verifyCountry(data.country);
 	const msgErrorCheckbox = verifyCheckbox([data.checkboxFirst[1], data.checkboxSecond[1]]);
-
+	const msgErrorCaptcha = verifyCaptcha(data.captcha);
 
 
 	msgErrorName ? nameElement.innerText = msgErrorName : null;
 	msgErrorCountry ? countryElement.innerText = msgErrorCountry : null;
 	msgErrorCheckbox ? checkboxElement.innerText = msgErrorCheckbox : null;
+	msgErrorCaptcha ? captchaElement.innerText = msgErrorCaptcha : null;
 	const accountArr = [data.checkboxFirst, data.checkboxSecond];
 	let accountStr = accountArr.filter(item => item[1] === true);
 
@@ -184,22 +257,22 @@ const formHandler = (e) => {
 	}
 	if (!msgErrorName && !msgErrorCountry && !msgErrorCheckbox) {
 		toggleLoader(true);
-		try {
-			fetch('https://global-ukraine-card.herokuapp.com/', {
-				method: 'POST',
-				body: JSON.stringify(postToAdd),
-				headers: {
-					'Content-Type': 'application/json; charset=UTF-8',
-				},
-			})
-				.then(response => response.json())
-				.then(post => successWindowOpen(post))
-				.catch(error => errorWindowOpen(error))
-				.finally(() => toggleLoader(false));;
-		} catch (e) {
-			errorWindowOpen(error);
-		}
+
+		const captchaObj = { captcha: data.captcha }
+		fetch('https://global-ukraine-card.herokuapp.com/captcha/', {
+			method: 'POST',
+			body: JSON.stringify(captchaObj),
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8',
+			},
+		})
+			.then(response => response.json())
+			.then(post => {
+				post.success ? fetchPostData(postToAdd, element) : errorWindowOpen(post.msg);
+			}).catch(error => errorWindowOpen(error)).finally(() => {
+				toggleLoader(false)
+				cleanInputs(element);
+			});
 	}
 }
-
 refs.form.addEventListener('submit', formHandler);
--- Global_Ukraine/global-ukraine-page/src/scripts/translation.js
+++ Global_Ukraine/global-ukraine-page/src/scripts/translation.js
@@ -4,8 +4,8 @@ const langArr = {
 		"en": "Ukrainians in Europe",
 	},
 	"possibility-text": {
-		"ua": "Ти маєш надзвичайну можливість відкриття євро рахунку в єврозоні - завдяки необанку Global Ukraine !",
-		"en": "You have an extraordinary opportunity to open a euro account in the eurozone - thanks to the neobank Global Ukraine!",
+		"ua": "Ви маєте надзвичайну можливість відкриття єврорахунку в Єврозоні - завдяки необанку Global Ukraine !",
+		"en": "You have an extraordinary opportunity to open a euro account in the Eurozone - thanks to the neobank Global Ukraine!",
 	},
 	"main-btn-1": {
 		"ua": "Оформити картку",
@@ -24,52 +24,52 @@ const langArr = {
 		"en": "Get a card",
 	},
 	"preferences-title": {
-		"ua": "Чому ми?",
-		"en": "Why us",
+		"ua": "Унікальні переваги",
+		"en": "Unique advantages",
 	},
 	"preferences-first-text": {
-		"ua": "Відкрити рахунок в Євро на основі українських документів",
-		"en": "Open an account in Euro based on Ukrainian documents",
+		"ua": "Відкрити Єврорахунок в Єврозоні на основі українських документів",
+		"en": "Open a Euro Account in the Eurozone based on Ukrainian documents",
 	},
 	"preferences-second-text": {
-		"ua": "Замовити банківську карту з підтримкою українською мовою",
-		"en": "Order a bank card with Ukrainian language support",
+		"ua": "Замовити Європейську банківську карту з підтримкою українською мовою",
+		"en": "Order a European bank card with Ukrainian language support",
 	},
 	"preferences-third-text": {
 		"ua": "Забезпечити фінансову безпеку своїм рідним, які в Україні",
 		"en": "Provide financial security to your relatives in Ukraine",
 	},
 	"chance-title": {
-		"ua": "Ваш шанс!",
-		"en": "Your chance!",
+		"ua": "Доступні фізособам та українським ФОПам в Європі",
+		"en": "Available to individuals and Ukrainian persons entrepreneurs (PE | FOP) in Europe",
 	},
 	"chance-text": {
 		"ua": "Хочете бути в першій тисячі українців, котрі матимуть банківську карту Global Ukraine?",
 		"en": "Do you want to be among the first thousand Ukrainians who will have a Global Ukraine bank card?",
 	},
 	"investments-title": {
-		"ua": "Ми інвестуємо!",
-		"en": "We invest!",
+		"ua": "Свій до свого по своє",
+		"en": "Building bridges and supporting communities",
 	},
 	"investments-text": {
-		"ua": "Global Ukraine працює під європейською ліцензією EMI для створення першого українського необанку в Європі, половина прибутку необанку буде спрямована на підтримку та захист українських громад в світі!",
-		"en": "Global Ukraine works under the European license of EMI to create the first Ukrainian neobank in Europe, half of the profit of the neobank will be directed to the support and protection of Ukrainian communities in the world!",
+		"ua": "Половина прибутку необанку буде спрямована на підтримку та захист українських громад в світі!",
+		"en": "Global Ukraine works under the European license of EMI to create the first Ukrainian neobank in Europe.<br>Half of the profit of the neobank will be directed to the support and protection of Ukrainian communities in the world!",
 	},
 	"form-title": {
-		"ua": "Введіть дані!",
+		"ua": "Зареєструватись",
 		"en": "Enter the data!",
 	},
 	"checkbox-first": {
-		"ua": "Індивідуальний (фізособа)",
-		"en": "Individual ",
+		"ua": "Індивідуальний рахунок (фізособа)",
+		"en": "Individual account",
 	},
 	"checkbox-second": {
-		"ua": "Корпоративний (ФОП)",
-		"en": "Corporative (PE)",
+		"ua": "Корпоративний рахунок (ФОП)",
+		"en": "Corporate account ( FOP|PE)",
 	},
 	"footer": {
-		"ua": "Платіжні рахунки та дебетові картки Visa випускаються Global Ukraine у партнерстві з LinkCy Payment Service Provider UAB, яка є Агентом Установи Електронних Грошей (LB001994), уповноваженою Банком Литви здійснювати емісію електронних  гроші та відповідні платіжні послуги.",
-		"en": "Payment accounts and Visa debit cards are issued by Global Ukraine in partnership with LinkCy Payment Service Provider UAB which is an Agent of an Electronic Money Institution (LB001994), authorized by the Bank of Lithuania for the issuing of electronic money and provision of the related payment services.",
+		"ua": "Карти Global Ukraine випускаються UAB PayrNet на підставі ліцензії Visa та управляються UAB LinkCy.<br>UAB PayrNet уповноважено Банком Литви відповідно до Закону про електронні гроші та установи електронних грошей (ref LB001994) для випуску електронних грошей і надання відповідних платіжних послуг.<br>UAB LinkCy є агентом UAB PayrNet, зареєстрованого в Банку Литви під номером 305756549.",
+		"en": "Global Ukraine Cards are issued by UAB PayrNet pursuant to a licence by Visa and managed by UAB LinkCy. <br>UAB PayrNet is authorised by the Bank of Lithuania under the Law on Electronic Money and Electronic Money Institutions (ref LB001994) for the issuing of electronic money and provision of the related payment services. <br>UAB LinkCy is an Agent of UAB PayrNet registered with the Bank of Lithuania under number 305756549.",
 	},
 }
 
