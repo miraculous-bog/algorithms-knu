5fa8d5bbf356c2e48a30223076ce17bcd7877fb2
Bogdan <bogdan9110@gmail.com>
2022-12-06 19:27:03 +0200

    update new version edit first

--- Global_Ukraine/global-ukraine-page/src/scripts/translation.js
+++ Global_Ukraine/global-ukraine-page/src/scripts/translation.js
@@ -23,6 +23,22 @@ const langArr = {
 		"ua": "Оформити картку",
 		"en": "Get a card",
 	},
+	"whom-title": {
+		"ua": "Для кого?",
+		"en": "For whom?",
+	},
+	"whom-first-text": {
+		"ua": "Українськи фопи в Європі, які працюють з Європейськими або міжнародними партнерами та прагнуть фінансової стабільності, без конверсійних комісій та обмежень національних банків.",
+		"en": "Українськи фопи в Європі, які працюють з Європейськими або міжнародними партнерами та прагнуть фінансової стабільності, без конверсійних комісій та обмежень національних банків.",
+	},
+	"whom-second-text": {
+		"ua": "Українці в Європі, які потребують європейського банківського рахунку для зручного управління операціями в євро.",
+		"en": "Українці в Європі, які потребують європейського банківського рахунку для зручного управління операціями в євро.",
+	},
+	"whom-third-text": {
+		"ua": "Всім українцям, хто підтримує створення української глобальної фінансової системи, з безкоштовними переказами, програмами лояльності та з можливістю генерувати соціальний кешбек для розвитку українських громад в світі.",
+		"en": "Всім українцям, хто підтримує створення української глобальної фінансової системи, з безкоштовними переказами, програмами лояльності та з можливістю генерувати соціальний кешбек для розвитку українських громад в світі.",
+	},
 	"preferences-title": {
 		"ua": "Унікальні переваги",
 		"en": "Unique advantages",
@@ -39,9 +55,25 @@ const langArr = {
 		"ua": "Забезпечити фінансову безпеку своїм рідним, які в Україні",
 		"en": "Provide financial security to your relatives in Ukraine",
 	},
-	"chance-title": {
-		"ua": "Доступні фізособам та українським ФОПам в Європі",
-		"en": "Available to individuals and Ukrainian persons entrepreneurs (PE | FOP) in Europe",
+	"take-title": {
+		"ua": "Як отримати рахунок та картку Global Ukraine?",
+		"en": "How to open the account and get the card Global Ukraine?",
+	},
+	"take-first-text": {
+		"ua": `Встановіть додаток "Global Ukraine Card" з App Store або Google Play.<br><br>Вкажіть свою адресу (постійну або тимчасову) в Європі.`,
+		"en": `Install the "Global Ukraine Card" app from App Store or Google Play. Indicate your address (permanent or temporary) in Europe.`,
+	},
+	"take-second-text": {
+		"ua": "Сфотографуйте свій паспорт і зробіть селфі. ",
+		"en": "Take a picture of your passport and take a selfie. ",
+	},
+	"take-third-text": {
+		"ua": "Активуйте свою віртуальну картку Global Ukraine VISA Debit і почніть здійснювати перекази або робити покупки онлайн.",
+		"en": "Активуйте свою віртуальну картку Global Ukraine VISA Debit і почніть здійснювати перекази або робити покупки онлайн.",
+	},
+	"take-fourth-text": {
+		"ua": "Замовте фізичну картку Global Ukraine VISA Debit (яку незабаром буде доставлено вам за вказаною європейською адресою). ",
+		"en": "Order your physical Global Ukraine VISA Debit card (which will soon be delivered to you at the European address indicated).",
 	},
 	"chance-text": {
 		"ua": "Хочете бути в першій тисячі українців, котрі матимуть банківську карту Global Ukraine?",
@@ -56,8 +88,8 @@ const langArr = {
 		"en": "Global Ukraine works under the European license of EMI to create the first Ukrainian neobank in Europe.<br>Half of the profit of the neobank will be directed to the support and protection of Ukrainian communities in the world!",
 	},
 	"form-title": {
-		"ua": "Зареєструватись",
-		"en": "Enter the data!",
+		"ua": "Завантажити",
+		"en": "Download",
 	},
 	"checkbox-first": {
 		"ua": "Індивідуальний рахунок (фізособа)",
@@ -67,9 +99,25 @@ const langArr = {
 		"ua": "Корпоративний рахунок (ФОП)",
 		"en": "Corporate account ( FOP|PE)",
 	},
+	"suport-title": {
+		"ua": "Служба підтримки ",
+		"en": "Helpdesk",
+	},
+	"program-title": {
+		"ua": "Програми лояльності",
+		"en": "Loyalty programs",
+	},
+	"program-text": {
+		"ua": `Український бізнес та міжнародні компанії можуть запропонувати власникам карток Global Ukraine Card програми лояльності, надсилаючи свої пропозиції поштою на <a href="maito:pr@global-ukraine.org">pr@global-ukraine.org.</a>`,
+		"en": `Ukrainian businesses and international companies can propose loyalty programs to the Global Ukraine Cards holders, by sending your offers by mail to  <a href="maito:pr@global-ukraine.org">pr@global-ukraine.org .</a>`,
+	},
+	"suport-text": {
+		"ua": "Спілкуйтеся з реальними людьми, які зроблять неможливе, щоб ефективно відповісти на ваші запитання через Месенджер",
+		"en": "Communicate with real people, who will do the impossible to answer your questions efficiently. Use your usual messengers",
+	},
 	"footer": {
-		"ua": "Карти Global Ukraine випускаються UAB PayrNet на підставі ліцензії Visa та управляються UAB LinkCy.<br>UAB PayrNet уповноважено Банком Литви відповідно до Закону про електронні гроші та установи електронних грошей (ref LB001994) для випуску електронних грошей і надання відповідних платіжних послуг.<br>UAB LinkCy є агентом UAB PayrNet, зареєстрованого в Банку Литви під номером 305756549.",
-		"en": "Global Ukraine Cards are issued by UAB PayrNet pursuant to a licence by Visa and managed by UAB LinkCy. <br>UAB PayrNet is authorised by the Bank of Lithuania under the Law on Electronic Money and Electronic Money Institutions (ref LB001994) for the issuing of electronic money and provision of the related payment services. <br>UAB LinkCy is an Agent of UAB PayrNet registered with the Bank of Lithuania under number 305756549.",
+		"ua": "&copy;Карти Global Ukraine випускаються UAB PayrNet на підставі ліцензії VISA та управляються UAB LinkCy.<br>UAB PayrNet уповноважено Банком Литви відповідно до Закону про електронні гроші та установи електронних грошей (ref LB001994) для випуску електронних грошей і надання відповідних платіжних послуг.<br>UAB LinkCy є агентом UAB PayrNet, зареєстрованого в Банку Литви під номером 305756549. ",
+		"en": "&copy;Global Ukraine Cards are issued by UAB PayrNet pursuant to a licence by VISA and managed by UAB LinkCy. <br>UAB PayrNet is authorised by the Bank of Lithuania under the Law on Electronic Money and Electronic Money Institutions (ref LB001994)  for the issuing of electronic money and provision of the related payment services.<br>UAB LinkCy is an Agent of UAB PayrNet registered with the Bank of Lithuania under number 305756549. ",
 	},
 }
 
